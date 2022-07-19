021ed16cd8a083b955a3107249138970435023bf
Lsege <sanabrialucas97@gmail.com>
2022-07-19 09:55:49 -0300

    Agregue lo antiguo

---
+++ Pascal/Holamundo.pas
@@ -0,0 +1,6 @@
+Program Holamundo;
+uses crt;
+begin
+	writeln('Holamundo');
+	readln(); // Para que no se cierre al ejecutar
+end.
---
+++ Pascal/grafico.pas
@@ -0,0 +1,13 @@
+Program grafico;
+uses crt;
+begin
+	writeln('   -30         0         30         60        90        120');
+	writeln('-20');
+	writeln('0              |');
+	writeln('1              |');
+	writeln('2');
+	writeln('-20');
+	writeln('-20');
+
+	readln(); // Para que no se cierre al ejecutar
+end.
